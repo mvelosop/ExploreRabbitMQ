952dcc67dfa31e1c6de36fa6ad12cfcb9d711046
Miguel Veloso <mvelosop@gmail.com>
2019-03-06 14:57:41 +0000

    Basic initial hello world program

---
+++ src/Send/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello RabbitMQ - SEND!");
+            Console.WriteLine("Write \"exit\" to exit");
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var message = "Hello RabbitMQ";
+
+                do
+                {
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(
+                        exchange: "",
+                        routingKey: "hello",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine($"- Sent \"{message}\"");
+
+                    message = Console.ReadLine();
+
+                } while (!message.Equals("exit", StringComparison.InvariantCultureIgnoreCase));
+
+            }
+        }
+    }
+}
